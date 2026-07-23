b5d966d4e97ac725d6fe391996b76a1a3794adcf
Parneet <parneetsingh022@gmail.com>
2026-07-23 11:15:06 +0530

    clerify panic while parsing int and float

--- src/parser/expression.rs
+++ src/parser/expression.rs
@@ -8,6 +8,8 @@ use crate::{
 
 impl<'a> Parser<'a> {
     fn parse_integer_literal(&self, symbol: Symbol, span: Span) -> Expr {
+        // It is okay to panic here, because this is not a user error. If lexer
+        // workes as intended, it would not lex any invalid IntLiteral.
         let value = self
             .symbol_registry
             .resolve(symbol)
@@ -18,6 +20,8 @@ impl<'a> Parser<'a> {
     }
 
     fn parse_float_literal(&self, symbol: Symbol, span: Span) -> Expr {
+        // It is okay to panic here, because this is not a user error. If lexer
+        // workes as intended, it would not lex any invalid FloatLiteral.
         let value = self
             .symbol_registry
             .resolve(symbol)
